5cb9412cdcf5026240a2a025be4686bb918eb88d
beiping <2016100827@ruc.edu.cn>
2017-12-19 14:39:20 +0800

    alter wenshu clean code

--- LawSpider/src/main/java/WenshuCourtSpider/Clean.java
+++ LawSpider/src/main/java/WenshuCourtSpider/Clean.java
@@ -2,8 +2,10 @@ package WenshuCourtSpider;
 
 import Interface.LawClean;
 import Interface.LawSpider;
+import Log.LawLogger;
 import Mongo.LawArticle;
 import Mongo.LawDocument;
+import org.apache.log4j.Logger;
 import org.bson.Document;
 import org.jsoup.Jsoup;
 
@@ -19,6 +21,8 @@ import java.util.List;
  * Modified By:
  */
 public class Clean extends LawClean {
+
+    private static Logger LOGGER = LawLogger.getLawLogger(Clean.class);
 
     public Clean(String crawJobCollection, String lawCollection, String cleanCollection) {
         super(crawJobCollection, lawCollection, cleanCollection);
@@ -52,6 +56,36 @@ public class Clean extends LawClean {
         String content = getContentHtmlBySelect(html);
         String cleanHtml = LawSpider.cleanHtml(content);
         String updateContent = getCleanContent(cleanHtml);
+        org.jsoup.nodes.Document doc = Jsoup.parse(html);
+
+        try {
+            String release_data = doc.select("#container > div > div > div.clearfix.detail_mes > ul > li.fl.print").first().childNode(0).toString();
+            release_data = release_data.trim().replace("发布时间：", "").replaceAll("&nbsp;", "");
+            law.put("release_date", release_data.trim());
+        } catch (NullPointerException e) {
+            LOGGER.warn("No release_data of content");
+        }
+
+        try {
+            String category = doc.select("#zoom > div:nth-child(3)").first().childNode(0).toString();
+            law.put("category", category.trim().replaceAll(" ", ""));
+        } catch (NullPointerException e) {
+            LOGGER.warn("No category of content");
+        }
+
+        try {
+            String department = doc.select("#zoom > div:nth-child(2)").first().childNode(0).toString();
+            law.put("department", department.trim());
+        } catch (NullPointerException e) {
+            LOGGER.warn("No department of content");
+        }
+
+        try {
+            String release_number = doc.select("#zoom > div:nth-child(4)").first().childNode(0).toString();
+            law.put("release_number", release_number.trim());
+        } catch (NullPointerException e) {
+            LOGGER.warn("No release_number of content");
+        }
 
         List<LawArticle> articleList = new ArrayList<LawArticle>();
         List<Document> interlDocuments = LawDocument.getArticleDocument(articleList);
