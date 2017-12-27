bd32383705cd6aa73dc81a567dd325c536b2d975
beiping <2016100827@ruc.edu.cn>
2017-12-27 22:44:15 +0800

    add code

--- LawSpider/src/main/java/ChinacourtSpider/Clean.java
+++ LawSpider/src/main/java/ChinacourtSpider/Clean.java
@@ -2,9 +2,16 @@ package ChinacourtSpider;
 
 import Interface.LawClean;
 import Log.LawLogger;
+import Mongo.MongoDB;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import org.apache.log4j.Logger;
 import org.bson.Document;
 import org.jsoup.Jsoup;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Description：
@@ -22,6 +29,18 @@ public class Clean extends LawClean {
         super(crawJobCollection, lawCollection, cleanCollection);
     }
 
+    public static boolean hasTitle(org.jsoup.nodes.Document doc) {
+        try {
+            if (doc.select("body > div.container > div > div.law_content > div > p:nth-child(1) > font").first().childNodes().size() == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (NullPointerException e) {
+            return false;
+        }
+    }
+
     public String getContentHtmlBySelect(String html) {
         try {
             org.jsoup.nodes.Document doc = Jsoup.parse(html);
@@ -35,21 +54,21 @@ public class Clean extends LawClean {
         String category = law.getString("category");
         law.put("level", "法律法规");
         law.put("timeless", "现行有效");
-        if(category.equals("司法解释")){
+        if (category.equals("司法解释")) {
             law.put("level", "司法解释");
         }
 
-        String title =  law.getString("title");
-        if(title == null){
+        String title = law.getString("title");
+        if (title == null) {
             String content = law.getString("content");
             String[] contentlist = content.split("\n");
             boolean find = false;
-            for (String par:contentlist) {
-                if(!find && par.contains("【文件来源】")){
+            for (String par : contentlist) {
+                if (!find && par.contains("【文件来源】")) {
                     find = true;
                     continue;
                 }
-                if(find && !par.trim().isEmpty()){
+                if (find && !par.trim().isEmpty()) {
                     law.put("title", par);
                     break;
                 }
@@ -59,4 +78,38 @@ public class Clean extends LawClean {
         super.cleanContent(law);
     }
 
+    public void CleanTitle(String collection) {
+        MongoDB mongoDB = MongoDB.getMongoDB();
+        MongoCollection<Document> cleanLawColletion = mongoDB.getCollection(collection);
+        FindIterable<Document> iterables = cleanLawColletion.find().noCursorTimeout(true).batchSize(10000);
+        MongoCursor<Document> cursor = iterables.iterator();
+        long num = 0;
+        try {
+            while (cursor.hasNext()) {
+                Document law = cursor.next();
+                num++;
+                String rawHtml = law.getString("rawHtml");
+                LOGGER.info("current Title: " + law.getString("title"));
+                String title = law.getString("title");
+                org.jsoup.nodes.Document doc = Jsoup.parse(rawHtml);
+                if (hasTitle(doc)) {
+                    title = doc.select("body > div.container > div > div.law_content > div > p:nth-child(1) > font").first().toString();
+                    LOGGER.info("find one title is wrong:" + law.getString("url"));
+                } else {
+                    LOGGER.info("title is right:" + law.getString("url"));
+                }
+                String regEx2_html = "<[^>]+>"; // 定义HTML标签的正则表达式
+                Pattern p2_html = Pattern.compile(regEx2_html, Pattern.CASE_INSENSITIVE);
+                Matcher m2_html = p2_html.matcher(title);
+                title = m2_html.replaceAll("").replaceAll(" ", "").trim(); // 过滤html标签
+                law.put("title", title);
+                updateDocumentContent(law);
+                LOGGER.info("current num: " + num);
+            }
+        } catch (Exception e) {
+            LOGGER.error("do clean title error: " + e.getMessage());
+        } finally {
+            cursor.close();
+        }
+    }
 }
