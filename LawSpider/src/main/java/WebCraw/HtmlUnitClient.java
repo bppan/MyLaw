3fc9efe4674d8871fbc32f4104da9afd9190eb16
beiping <2016100827@ruc.edu.cn>
2017-12-28 21:21:00 +0800

    add code

--- LawSpider/src/main/java/WebCraw/HtmlUnitClient.java
+++ LawSpider/src/main/java/WebCraw/HtmlUnitClient.java
@@ -1,44 +1,58 @@
 package WebCraw;
 
+import Log.LawLogger;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.CookieManager;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
 import org.apache.commons.logging.LogFactory;
-
-import java.util.logging.Level;
+import org.apache.log4j.Logger;
 
 /**
  * @Author : Administrator
  * @Date : 2017/11/16 21:09
  * @Description :
  */
+
 public class HtmlUnitClient {
+    private static Logger LOGGER = LawLogger.getLawLogger(HtmlUnitClient.class);
     private WebClient webClient;
 
     private HtmlUnitClient() {
         LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
-        System.out.println("load htmlUnitClient begin...");
-        webClient = new WebClient(BrowserVersion.CHROME);
-        // 这里是配置一下不加载css和加载javaScript
-        webClient.getOptions().setJavaScriptEnabled(true); // 启用JS解释器，默认为true
-        webClient.getOptions().setCssEnabled(false); // 禁用css支持
-        //启动cookie管理
-        webClient.setCookieManager(new CookieManager());
-        //启动ajax代理
-        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-        webClient.getOptions().setThrowExceptionOnScriptError(false); // js运行错误时，是否抛出异常
-        webClient.getOptions().setTimeout(50000); // 设置连接超时时间 ，这里是50S。如果为0，则无限期等待
-        System.out.println("load htmlUnitClient success...");
+        LOGGER.info("load htmlUnitClient begin...");
+        this.webClient = getNewHtmlUnitClient(0);
+        LOGGER.info("load htmlUnitClient success...");
     }
 
     public static WebClient getSingletonHtmlUntiClent() {
         return SingletonHelp.instance.webClient;
     }
 
+    public static WebClient getNewHtmlUnitClient(int webClient) {
+        WebClient newClient;
+        if (webClient == 0) {
+            newClient = new WebClient(BrowserVersion.CHROME);
+        } else if (webClient == 1) {
+            newClient = new WebClient(BrowserVersion.FIREFOX_45);
+        } else {
+            newClient = new WebClient(BrowserVersion.INTERNET_EXPLORER);
+        }
+        newClient.getOptions().setJavaScriptEnabled(true); // 启用JS解释器，默认为true
+        newClient.getOptions().setCssEnabled(false); // 禁用css支持
+        //启动cookie管理
+        newClient.setCookieManager(new CookieManager());
+        //启动ajax代理
+        newClient.setAjaxController(new NicelyResynchronizingAjaxController());
+        newClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        newClient.getOptions().setThrowExceptionOnScriptError(false); // js运行错误时，是否抛出异常
+        newClient.getOptions().setTimeout(50000); // 设置连接超时时间 ，这里是50S。如果为0，则无限期等待
+        return newClient;
+    }
+
     private static class SingletonHelp {
         private static HtmlUnitClient instance = new HtmlUnitClient();
     }
+
 
 }
