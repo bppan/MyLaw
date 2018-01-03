648cee085c2136986dce8f00a85b108c40260cdd
beiping <2016100827@ruc.edu.cn>
2018-01-03 16:46:52 +0800

    add solrWeb

--- src/main/java/Test3.java
+++ src/main/java/Test3.java
@@ -1,29 +1,14 @@
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Label;
-import org.neo4j.io.fs.FileUtils;
 
 import java.io.File;
-import java.io.IOException;
 
 public class Test3 {
-
-    public enum Tutorials implements Label {
-        JAVA,SCALA,SQL,NEO4J;
-    }
-
-    public enum TutorialRelationships implements RelationshipType{
-        JVM_LANGIAGES,NON_JVM_LANGIAGES;
-    }
 
     public static void main(String[] args) {
         GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
         File dbfile = new File("graph.db");
-        GraphDatabaseService db= dbFactory.newEmbeddedDatabase(dbfile);
+        GraphDatabaseService db = dbFactory.newEmbeddedDatabase(dbfile);
         try (Transaction tx = db.beginTx()) {
             Node javaNode = db.createNode(Tutorials.JAVA);
             javaNode.setProperty("TutorialID", "JAVA001");
@@ -38,13 +23,21 @@ public class Test3 {
             scalaNode.setProperty("Status", "Completed");
 
             Relationship relationship = javaNode.createRelationshipTo
-                    (scalaNode,TutorialRelationships.JVM_LANGIAGES);
-            relationship.setProperty("Id","1234");
-            relationship.setProperty("OOPS","YES");
-            relationship.setProperty("FP","YES");
+                    (scalaNode, TutorialRelationships.JVM_LANGIAGES);
+            relationship.setProperty("Id", "1234");
+            relationship.setProperty("OOPS", "YES");
+            relationship.setProperty("FP", "YES");
 
             tx.success();
         }
         System.out.println("Done successfully");
+    }
+
+    public enum Tutorials implements Label {
+        JAVA, SCALA, SQL, NEO4J;
+    }
+
+    public enum TutorialRelationships implements RelationshipType {
+        JVM_LANGIAGES, NON_JVM_LANGIAGES;
     }
 }
