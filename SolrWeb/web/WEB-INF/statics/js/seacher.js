d7eef3bb1ba48bd4cfa303a70afd20125e13f584
beiping <2016100827@ruc.edu.cn>
2018-01-16 14:32:39 +0800

    add code

--- SolrWeb/web/WEB-INF/statics/js/seacher.js
+++ SolrWeb/web/WEB-INF/statics/js/seacher.js
@@ -149,7 +149,7 @@ function getContent(resultList) {
         html += "<div class='row' style='margin-bottom: 15px'><div class='col-md-12'>" +
             "<h4 style='margin-bottom: 5px; font-family: arial;'><a href=" + resultList[i].url + " target='_blank' style='color:#1a0dab'>" + resultList[i].title + "</a></h4>" +
             "<p style='font-size: 13px; font-family: arial;line-height: 1.4; word-wrap: break-word; word-break: break-word; margin-top: 0; margin-bottom: 5px;'>" + resultList[i].content + "...</p>" +
-            "<p class='pull-left' style='margin-top: 0;margin-bottom: 5px; font-family: arial;'>" +
+            "<p class='pull-left' style='margin-top: 0;margin-bottom: 3px; font-family: arial;'>" +
             "<span class='label label-default' style='color:#545454; padding-left: 0;padding-right.9em; display:block;float:left;'>[发文字号]" + resultList[i].release_number + "</span>" +
             "<span class='label label-default' style='color:#545454; padding-left: 0;padding-right.9em; display:block;float:left;'>[发布日期]" + resultList[i].release_date + "</span>" +
             "<span class='label label-default' style='color:#545454; padding-left: 0;padding-right.9em; display:block;float:left;'>[实施日期]" + resultList[i].implement_date + "</span>" +
@@ -157,9 +157,8 @@ function getContent(resultList) {
             "<span class='label label-default' style='color:#545454; padding-left: 0;padding-right.9em; display:block;float:left;'>[法规级别]" + resultList[i].level + "</span>" +
             "<span class='label label-default' style='color:#545454; padding-left: 0;padding-right.9em; display:block;float:left;'>[时效性]" + resultList[i].timeless + "</span>" +
             "</p>" +
-            "<ul class='list-inline' style='margin: 0;padding: 0;'>" +
-            "<li style='padding-left: 0;'>" +
-            "<a href=" + resultList[i].url + " target='_blank' style='color:#006621;word-break:break-all;'>" + resultList[i].url + "</a></li></ul>" +
+            "<p class='pull-left' style='margin: 0;padding: 0;font-size: 14px;line-height: 1.4; '>" +
+            "<a href=" + resultList[i].url + " target='_blank' style='color:green;word-break:break-all;'>" + resultList[i].url + "</a></p>" +
             "</div></div>";
     }
     return html;
