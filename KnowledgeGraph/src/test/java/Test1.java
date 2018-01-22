30cb7d1dbe2c4a3d627a06a022b6c14b4a466aa8
beiping <2016100827@ruc.edu.cn>
2018-01-22 15:59:47 +0800

    add code

---
+++ KnowledgeGraph/src/test/java/Test1.java
@@ -0,0 +1,15 @@
+import util.NumberChange;
+
+/**
+ * Description：
+ * Author: Administrator
+ * Created:  2018/1/22 14:48
+ * Copyright: Copyright (c) 2017
+ * Version: 0.0.1
+ * Modified By:
+ */
+public class Test1 {
+    public static void main(String[] args){
+        System.out.println(NumberChange.numberToChinese(7));
+    }
+}
