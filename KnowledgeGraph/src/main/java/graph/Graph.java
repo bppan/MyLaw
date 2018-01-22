5f3ced5daa2da71f52f9d985dfbea2fe49046ba6
beiping <2016100827@ruc.edu.cn>
2018-01-22 16:52:29 +0800

    add code

--- KnowledgeGraph/src/main/java/graph/Graph.java
+++ KnowledgeGraph/src/main/java/graph/Graph.java
@@ -10,6 +10,7 @@ import org.apache.log4j.Logger;
 import org.bson.Document;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.Session;
+import util.NumberChange;
 
 import java.util.List;
 
@@ -34,84 +35,159 @@ public class Graph {
         graph.importDataToGraph("law");
     }
 
-    public void createNode(Document law) {
+    public boolean createNode(Document law) {
         Session session = driver.session();
         try {
-            StringBuilder createNodecyphe = new StringBuilder("CREATE (n:law {");
+            StringBuilder createNodecyphe = new StringBuilder("MERGE (n:law {");
             createNodecyphe.append("name:'").append(law.getString("title")).append("'");
             createNodecyphe.append(", id:'").append(law.getObjectId("_id").toString()).append("'");
             if (law.getString("department") != null && !law.getString("department").isEmpty()) {
-                createNodecyphe.append(", 发布单位:'").append(law.getString("department")).append("'");
+                createNodecyphe.append(", department:'").append(law.getString("department")).append("'");
             }
             if (law.getString("release_date") != null && !law.getString("release_date").isEmpty()) {
-                createNodecyphe.append(", 发布日期:'").append(law.getString("release_date")).append("'");
+                createNodecyphe.append(", release_date:'").append(law.getString("release_date")).append("'");
             }
             if (law.getString("release_number") != null && !law.getString("release_number").isEmpty()) {
-                createNodecyphe.append(", 文号:'").append(law.getString("release_number")).append("'");
+                createNodecyphe.append(", release_number:'").append(law.getString("release_number")).append("'");
             }
             if (law.getString("implement_date") != null && !law.getString("implement_date").isEmpty()) {
-                createNodecyphe.append(", 生效日期:'").append(law.getString("implement_date")).append("'");
+                createNodecyphe.append(", implement_date:'").append(law.getString("implement_date")).append("'");
             }
             if (law.getString("category") != null && !law.getString("category").isEmpty()) {
-                createNodecyphe.append(", 类别:'").append(law.getString("category")).append("'");
+                createNodecyphe.append(", category:'").append(law.getString("category")).append("'");
             }
             if (law.getString("level") != null && !law.getString("level").isEmpty()) {
-                createNodecyphe.append(", 法律级别:'").append(law.getString("level")).append("'");
+                createNodecyphe.append(", level:'").append(law.getString("level")).append("'");
             }
             if (law.getString("timeless") != null && !law.getString("timeless").isEmpty()) {
-                createNodecyphe.append(", 时效性:'").append(law.getString("timeless")).append("'");
-            }
-            if (law.getString("content") != null && !law.getString("content").isEmpty()) {
-                createNodecyphe.append(", 内容:'").append(law.getString("content")).append("'");
+                createNodecyphe.append(", timeless:'").append(law.getString("timeless")).append("'");
             }
             if (law.getInteger("article_num") != null) {
-                createNodecyphe.append(", 法条数:").append(law.getInteger("article_num"));
+                createNodecyphe.append(", article_num:").append(law.getInteger("article_num"));
             }
             createNodecyphe.append("})");
             session.run(createNodecyphe.toString());
             LOGGER.info("create node done: " + law.getString("title"));
+            return true;
         } catch (Exception e) {
             LOGGER.info("create node in neo4j err: " + e);
+            return false;
         } finally {
             session.close();
-        }
-
-    }
-
-    public void createChildNode() {
-        Session session = driver.session();
-
-    }
-
-    public void createRelationShip(Document law) {
-        List<Document> documentList = (List<Document>) law.get("articles");
-        if (documentList.size() == 0) {
-            return;
         }
     }
 
     @SuppressWarnings("unchecked")
+    public boolean createChildNode(Document law) {
+        Session session = driver.session();
+        try {
+            List<Document> documentList = (List<Document>) law.get("articles");
+            if (documentList.size() == 0) {
+                LOGGER.info("the law have no child");
+                return true;
+            }
+            if (documentList.size() == 1 && documentList.get(0).getString("name").trim().isEmpty()) {
+                LOGGER.info("the law have no child");
+                return true;
+            }
+
+            for (int i = 0; i < documentList.size(); i++) {
+                //创建法条
+                String name = documentList.get(i).getString("name");
+                String childId = law.getObjectId("_id").toString() + "-" + i;
+                StringBuilder createNodecyphe = new StringBuilder("MERGE (n:article {");
+                createNodecyphe.append("name:'").append(name).append("'");
+                createNodecyphe.append(", id:'").append(childId).append("'");
+                createNodecyphe.append("})");
+                session.run(createNodecyphe.toString());
+                //创建法条款项
+                List<String> para = (List<String>) documentList.get(i).get("paragraph");
+                if (para.size() <= 1) {
+                    continue;
+                }
+                for (int j = 0; j < para.size(); j++) {
+                    String childName = "第" + NumberChange.numberToChinese(j + 1) + "款";
+                    String childChildId = childId + "-" + j;
+                    StringBuilder createChildNodecyphe = new StringBuilder("MERGE (n:paragraph {");
+                    createChildNodecyphe.append("name:'").append(childName).append("'");
+                    createChildNodecyphe.append(", id:'").append(childChildId).append("'");
+                    createChildNodecyphe.append("})");
+                    session.run(createChildNodecyphe.toString());
+                }
+            }
+            LOGGER.info("create first layer child node done: " + law.getString("title"));
+            return true;
+        } catch (Exception e) {
+            LOGGER.info("create first layer child node in neo4j err: " + e);
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean createRelationship(Document law) {
+        Session session = driver.session();
+        try {
+            List<Document> documentList = (List<Document>) law.get("articles");
+            if (documentList.size() == 0) {
+                LOGGER.info("the law have no child relationship");
+                return true;
+            }
+            if (documentList.size() == 1 && documentList.get(0).getString("name").trim().isEmpty()) {
+                LOGGER.info("the law have no child relationship");
+                return true;
+            }
+            for (int i = 0; i < documentList.size(); i++) {
+                //创建法律和法条
+                StringBuilder createNodecyphe = new StringBuilder("MATCH (a:law {");
+                createNodecyphe.append("id: '").append(law.getObjectId("_id").toString()).append("'}), ");
+                String childId = law.getObjectId("_id").toString() + "-" + i;
+                createNodecyphe.append("(b:article {");
+                createNodecyphe.append("id: '").append(childId).append("'}) ");
+                createNodecyphe.append("MERGE (a)").append("-[:").append("有").append("]").append("->(b)");
+                session.run(createNodecyphe.toString());
+                //创建法条款项关系
+                List<String> para = (List<String>) documentList.get(i).get("paragraph");
+                if (para.size() <= 1) {
+                    continue;
+                }
+                for (int j = 0; j < para.size(); j++) {
+                    StringBuilder createNodeChildRelationshipcyphe = new StringBuilder("MATCH (a:article {");
+                    createNodeChildRelationshipcyphe.append("id: '").append(childId).append("'}), ");
+                    String childChildId = childId + "-" + j;
+                    createNodeChildRelationshipcyphe.append("(b:paragraph {");
+                    createNodeChildRelationshipcyphe.append("id: '").append(childChildId).append("'}) ");
+                    createNodeChildRelationshipcyphe.append("MERGE (a)").append("-[:").append("有").append("]").append("->(b)");
+                    session.run(createNodeChildRelationshipcyphe.toString());
+                }
+            }
+            LOGGER.info("create relationShip done: " + law.getString("title"));
+            return true;
+        } catch (Exception e) {
+            LOGGER.info("create child node relationShip in neo4j err: " + e);
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
     public void importDataToGraph(String collectionName) {
         MongoServer mongoServer = MongoServer.getMongoDB();
         MongoCollection<Document> collection = mongoServer.getCollection(collectionName);
-        FindIterable<Document> iterables = collection.find(new Document("url", "http://www.pkulaw.cn/fulltext_form.aspx?Db=chl&Gid=401d47f76b65500d&keyword=&EncodingName=&Search_Mode=accurate&Search_IsTitle=0")).noCursorTimeout(true).batchSize(10000);
+        FindIterable<Document> iterables = collection.find().noCursorTimeout(true).batchSize(10000);
         MongoCursor<Document> cursor = iterables.iterator();
         try {
             while (cursor.hasNext()) {
                 Document law = cursor.next();
-//                List<Document> documentList = (List<Document>)law.get("articles");
-//                for (Document document:documentList) {
-//                    if(document.getString("name").isEmpty()){
-//                        System.out.println("dddddddd");
-//                    }
-//                    System.out.println(document.getString("name"));
-//                }
-//                this.createNode(law);
+                this.createNode(law);
+                this.createChildNode(law);
+                this.createRelationship(law);
             }
         } catch (Exception e) {
             LOGGER.info("read data from mongodb err: " + e);
         } finally {
-//            driver.close();
+            driver.close();
             cursor.close();
         }
     }
