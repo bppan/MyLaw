bc686d156cd5325e24ddacb638f0ff8244804dfd
beiping <2016100827@ruc.edu.cn>
2018-02-03 17:03:49 +0800

    add code

---
+++ KnowledgeGraph/src/main/java/graph/MainGraphSynchroMongo.java
@@ -0,0 +1,16 @@
+package graph;
+
+/**
+ * Description：
+ * Author: beiping_pan
+ * Created:  2018/2/3 15:15
+ * Copyright: Copyright (c) 2017
+ * Version: 0.0.1
+ * Modified By:
+ */
+public class MainGraphSynchroMongo {
+    public static void main(String[] args){
+
+    }
+
+}
